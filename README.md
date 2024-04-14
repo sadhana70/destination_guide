a4246cef4c4a272dc10f3883cf5165338aea2d49
sadhanapanthi <panthisadhana123@gmail.com>
2024-04-14 10:33:44 +0545

    frontend complete

---
+++ README.md
@@ -0,0 +1,5 @@
+# destination_guide
+
+A Flutter project working as a guide for tourists.
+
+<img src="ss.png" alt="lake image" width="300">
---
+++ android/app/src/main/kotlin/com/example/destination_guide/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.destination_guide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
